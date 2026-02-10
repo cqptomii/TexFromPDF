d0edcb912539094f332fd794bbf930fb06d51721
Tom <150679896+cqptomii@users.noreply.github.com>
2026-02-10 01:56:18 +0100

    feat: Add numpy encoders

---
+++ src/utils/numpy_encoder.py
@@ -0,0 +1,11 @@
+import json, numpy as np
+
+class NumpyJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NumpyJSONEncoder, self).default(obj)
